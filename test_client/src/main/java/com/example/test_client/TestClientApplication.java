fd910d2608d363099bfbcfa3eb9846058e32586e
sumanthbalaji <sumanth@sentisum.com>
2022-12-05 23:27:59 +0530

    all the required servers

---
+++ test_client/src/main/java/com/example/test_client/TestClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestClientApplication.class, args);
+	}
+
+}
---
+++ test_client/src/test/java/com/example/test_client/TestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
